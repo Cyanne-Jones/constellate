02b996ea3c534c5610a046fb684a5793a046f051
Cyanne-Jones <jonescyanne@yahoo.com>
2022-09-19 11:21:11 -0600

    Create rudimentary file structure

---
+++ components/Nav.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const navLinks = [
+
+  { title: 'home', path: '/' },
+  { title: 'create', path: '/create' },
+  { title: 'login', path: '/login' }
+
+];
+
+export default function Nav() {
+
+  const router= useRouter();
+
+  return (
+    <div>
+      {navLinks.map(link => (
+        <Link href={link.path} passHref>
+          <a>
+            {link.title}
+          </a>
+        </Link>
+      ))}
+    </div>
+  )
+
+};
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Nav from '../components/Nav';
+
+export default function Home() {
+
+  return (
+    <>
+    <Head>
+      <title>The Write Stuff</title>
+    </Head>
+      <Nav />
+    </>
+  )
+};
