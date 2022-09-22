034babe8848723144eef8ffa65c4ea9320545807
Cyanne-Jones <jonescyanne@yahoo.com>
2022-09-22 13:42:09 -0600

    Style calendar

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -6,19 +6,29 @@ import styles from '../styles/Calendar.module.css';
 import Calendar from 'react-calendar';
 import { db, auth } from '../firebase-config';
 import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import useIsAuthStore from '../state/useIsAuthStore';
+import { useRouter } from 'next/router';
 
 export default function CalendarPage() {
 
   const [ selectedDate, setSelectedDate ] = useState(new Date());
   const [ entryList, setEntryList ] = useState([]);
   const entryCollectionRef = collection(db, 'entries');
+  const isAuth = useIsAuthStore(state => state.isAuth);
+  const router = useRouter();
 
   useEffect(() => {
 
     const getEntries = async () => {
 
+      if (!isAuth) {
+        setTimeout(() => router.push('/login'), 3000);
+        return;
+      }
+
       const data = await getDocs(entryCollectionRef);
-      setEntryList(data.docs.map(data => ({...data.data(), id: data.id})));
+      setEntryList(data.docs.map(data => ({...data.data(), id: data.id}))
+      .filter(entry => entry.author.id === auth.currentUser.uid));
       console.log(entryList);
 
     };
@@ -35,14 +45,24 @@ export default function CalendarPage() {
     
     if (view === 'month') {
 
-      const newEntry = entryList.find(entry => (new Date(entry.dateCreated).getDate() === date.getDate()));
+      const newEntry = entryList.find(entry => 
+        (new Date(entry.dateCreated).getDate() === date.getDate()) && 
+        (new Date(entry.dateCreated).getMonth() === date.getMonth()) && 
+        (new Date(entry.dateCreated).getYear() === date.getYear()));
 
       if (newEntry) {
 
         return (
-          <p className={styles.calendarEntry} style={{backgroundColor: newEntry.color}}>{newEntry.title}</p>
+          <div className={styles.tile}>
+            <p
+              className={styles.calendarEntry} 
+              style={{backgroundColor: newEntry.color}}
+              onClick={() => router.push('/create')}
+            >
+              {newEntry.title}
+            </p>
+          </div>
         );
-
        };
 
     }
@@ -54,14 +74,24 @@ export default function CalendarPage() {
     <Head>
       <title>Calendar</title>
     </Head>
-      <Nav />
-      Calendar
-      <Calendar 
-        onChange={setSelectedDate} 
-        value={selectedDate}
-        tileContent={tileContent}
-        className={styles.reactCalendar}
-      />
+      <div className={styles.calendar}>
+        <Nav />
+        {!isAuth ? 
+        <div className={styles.errorContainer}>
+          <h1 className={styles.errorMessage}>
+            Please sign in to create a new journal entry
+          </h1> 
+        </div> :
+        <div className={styles.calendarContainer}>
+          <Calendar 
+            onChange={setSelectedDate} 
+            value={selectedDate}
+            tileContent={tileContent}
+            className={styles.reactCalendar}
+          />
+        </div>
+        }
+      </div>
     </>
   )
   
