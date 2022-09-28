ffc9cbe0d8754de8771f0d52be29cc97deaa0c77
Cyanne-Jones <jonescyanne@yahoo.com>
2022-09-28 11:53:18 -0600

    Set default to menu closed

--- state/useHamburgerOnStore.js
+++ state/useHamburgerOnStore.js
@@ -1,7 +1,7 @@
 import create from 'zustand';
 
 const useHamburgerOnStore = create((set) => ({
-  isMenuOpen: true,
+  isMenuOpen: false,
   setIsMenuOpen: (bool) => set({ isMenuOpen: bool })
 }));
 
