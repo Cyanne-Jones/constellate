4be915bd69166f0ac972512b471bd8d760afb406
Cyanne-Jones <jonescyanne@yahoo.com>
2022-10-12 11:56:09 -0600

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
       <link href="https://fonts.googleapis.com/css2?family=Antonio&display=swap" rel="stylesheet"></link>
+      <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
       </Head>
       <body>
         <Main />
